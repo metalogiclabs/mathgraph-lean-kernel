91ba5db4029290b257a08494559ec3283cddbee3
Heath Sanchez <32909488+heathsanchez@users.noreply.github.com>
2026-09-11 08:14:56 +1200

    Add wide framed environments for exact deep read sets

--- src/value.rs
+++ src/value.rs
@@ -293,6 +293,14 @@ pub enum Env<'a> {
         len: u32,
         prune: Cell<(u64, Option<E<'a>>)>,
     },
+    WideFramed {
+        words: &'a [u64],
+        slots: &'a [V<'a>],
+        lsub: Option<&'a LevelSub<'a>>,
+        hash: u64,
+        len: u32,
+        prune: Cell<(u64, Option<E<'a>>)>,
+    },
 }
 
 pub fn lsub_key(lsub: Option<&LevelSub<'_>>) -> u64 {
@@ -306,7 +314,7 @@ impl<'a> Env<'a> {
     #[inline]
     pub fn get_hash(&self) -> u64 {
         match self {
-            Env::Nil { hash, .. } | Env::Cons { hash, .. } | Env::Framed { hash, .. } => *hash,
+            Env::Nil { hash, .. } | Env::Cons { hash, .. } | Env::Framed { hash, .. } | Env::WideFramed { hash, .. } => *hash,
         }
     }
 
@@ -314,14 +322,14 @@ impl<'a> Env<'a> {
     pub fn len(&self) -> u32 {
         match self {
             Env::Nil { .. } => 0,
-            Env::Cons { len, .. } | Env::Framed { len, .. } => *len,
+            Env::Cons { len, .. } | Env::Framed { len, .. } | Env::WideFramed { len, .. } => *len,
         }
     }
 
     #[inline]
     pub fn lsub(&self) -> Option<&'a LevelSub<'a>> {
         match self {
-            Env::Nil { lsub, .. } | Env::Cons { lsub, .. } | Env::Framed { lsub, .. } => *lsub,
+            Env::Nil { lsub, .. } | Env::Cons { lsub, .. } | Env::Framed { lsub, .. } | Env::WideFramed { lsub, .. } => *lsub,
         }
     }
 
@@ -396,6 +404,17 @@ impl<'a> Env<'a> {
                     }
                     let below = mask & ((1u64 << idx) - 1);
                     return Some(slots[below.count_ones() as usize]);
+                }
+                Env::WideFramed { words, slots, .. } => {
+                    let wi = usize::from(idx) / 64;
+                    let bi = u32::from(idx % 64);
+                    let Some(&word) = words.get(wi) else { return None };
+                    if (word >> bi) & 1 == 0 {
+                        return None;
+                    }
+                    let before_words: usize = words[..wi].iter().map(|w| w.count_ones() as usize).sum();
+                    let below = if bi == 0 { 0 } else { word & ((1u64 << bi) - 1) };
+                    return Some(slots[before_words + below.count_ones() as usize]);
                 }
             }
         }
