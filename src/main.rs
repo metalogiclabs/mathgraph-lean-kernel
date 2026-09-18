edc1a0523a446fec5fe65a91a7da3439dae4df2d
Heath Sanchez <32909488+heathsanchez@users.noreply.github.com>
2026-09-18 23:32:26 +1200

    Emit QCKN eval atlas in profiling builds

--- src/main.rs
+++ src/main.rs
@@ -27,6 +27,8 @@ fn main() {
             }
         }
     };
+    #[cfg(feature = "qckn-eval-atlas")]
+    sokonanoda::eval::dump_eval_atlas();
     match out {
         Ok(Some(msg)) => println!("{}", msg),
         Ok(None) => {}
