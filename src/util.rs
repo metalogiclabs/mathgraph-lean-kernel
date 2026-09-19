9e3f96371008aa0d15f2411ac18ae450e741629b
Heath Sanchez <32909488+heathsanchez@users.noreply.github.com>
2026-09-19 13:07:54 +1200

    Add optional exact neutral-app front cache

--- src/util.rs
+++ src/util.rs
@@ -1034,6 +1034,7 @@ pub struct NameCache<'p> {
 
 pub(crate) const PRUNE_DM_LEN: usize = 1 << 10;
 pub(crate) const PRUNE_DM_SHIFT: u32 = 64 - 10;
+pub(crate) const APP_FRONT_DM_LEN: usize = 1 << 16;
 
 pub struct TcCache<'a, 't> {
     pub(crate) unfold_const_cache: FxHashMap<(NamePtr<'t>, LevelsPtr<'t>), V<'a>>,
@@ -1059,6 +1060,7 @@ pub struct TcCache<'a, 't> {
     pub(crate) bvar_hc: FxHashMap<(u32, usize), V<'a>>,
     pub(crate) spine_hc: FxHashMap<(usize, u64), S<'a>>,
     pub(crate) app_hc: FxHashMap<(usize, usize), V<'a>>,
+    pub(crate) app_front_dm: Option<Box<[(usize, usize, Option<V<'a>>); APP_FRONT_DM_LEN]>>,
     pub(crate) env_hc: FxHashMap<(usize, usize), E<'a>>,
     pub(crate) lam_hc: FxHashMap<(ExprPtr<'t>, usize, ExprPtr<'t>), V<'a>>,
     pub(crate) pi_hc: FxHashMap<(usize, usize, ExprPtr<'t>, usize), V<'a>>,
@@ -1111,6 +1113,11 @@ impl<'a, 't> TcCache<'a, 't> {
             bvar_hc: session_small_fx_hash_map(),
             spine_hc: session_fx_hash_map(),
             app_hc: session_fx_hash_map(),
+            app_front_dm: if crate::flash::APP_FRONT_DM {
+                Some(Box::new([(0, 0, None); APP_FRONT_DM_LEN]))
+            } else {
+                None
+            },
             env_hc: session_fx_hash_map(),
             lam_hc: session_small_fx_hash_map(),
             pi_hc: session_small_fx_hash_map(),
@@ -1161,6 +1168,9 @@ impl<'a, 't> TcCache<'a, 't> {
         self.bvar_hc.clear();
         self.spine_hc.clear();
         self.app_hc.clear();
+        if let Some(dm) = self.app_front_dm.as_mut() {
+            dm.fill((0, 0, None));
+        }
         self.env_hc.clear();
         self.lam_hc.clear();
         self.pi_hc.clear();
@@ -1208,6 +1218,9 @@ impl<'a, 't> TcCache<'a, 't> {
         shrink_map(&mut self.bvar_hc);
         shrink_map(&mut self.spine_hc);
         shrink_map(&mut self.app_hc);
+        if let Some(dm) = self.app_front_dm.as_mut() {
+            dm.fill((0, 0, None));
+        }
         shrink_map(&mut self.env_hc);
         shrink_map(&mut self.lam_hc);
         shrink_map(&mut self.pi_hc);
